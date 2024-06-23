4f0669c61d1199d480a8d5d1b435677d3fd02645
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-06-23 15:52:36 +0300

    Добавьте файлы проекта.

---
+++ Lesson24/Program.cs
@@ -0,0 +1,41 @@
+﻿//Тема 24. Рекурсия. Домашнее задание.
+//Console.Write("Введите слово: ");
+//string word = Console.ReadLine();
+//if (IsPalindrome(word))
+//{
+//    Console.WriteLine("Is a palindrom");
+//}
+//else
+//{
+//    Console.WriteLine("Not a palindrom");
+//}
+//bool IsPalindrome(string word)
+//{
+//    if (word.Length <= 1)
+//        return true;
+
+//    // Сравниваем первый и последний символы
+//    if (word[0] == word[word.Length - 1])
+//    {
+//        // Рекурсивный вызов для оставшейся части строки
+//        return IsPalindrome(word.Substring(1, word.Length - 2));
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+//Тема 24. Рекурсия. Домашнее задание.
+//Урок 2/2
+//Console.Write("Введите любое положительное число:");
+//int n=int.Parse(Console.ReadLine()!);
+//PrintOddNumbers(n);
+//void PrintOddNumbers(int n)
+//{
+//        if (n <= 0) return;
+//    PrintOddNumbers(n - 1);
+//    if (n % 2 != 0)
+//    {
+//        Console.WriteLine(n);
+//    }
+//}
